bd9489e47607df57c0ad76fe6c3565f1c13bcf7f
mawguai <mawguai@gmail.com>
2018-05-14 00:37:03 +0200

    step-3

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+var vertexShaderText =
+    [
+        'precision mediump float;',
+        '',
+        'attribute vec2 vertPosition;',
+        '',
+        'void main()',
+        '{',
+        '   gl_Position = vec4(vertPosition, 0.0, 1.0);',
+        '}'
+    ].join('\n');
+
+var fragmentShaderText =
+    [
+        'precision mediump float;',
+        '',
+        'void main()',
+        '{',
+        '   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+        '}'
+    ].join('\n');
+
 var initDemo = function() {
 
     var canvas = document.getElementById('webgl-triangle');
@@ -7,6 +29,65 @@ var initDemo = function() {
         alert('Your browser does not support WebGl');
     }
 
-    gl.clearColor(R, G, B, A); // Set the values you want
-    gl.clear(); // Initialize both the depth && color buffer
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderText);
+    gl.shaderSource(fragmentShader, fragmentShaderText);
+
+    gl.compileShader(vertexShader);
+
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        console.error('ERROR compiling vertex Shader', gl.getShaderInfoLog(vertexShader));
+        return;
+    }
+    gl.compileShader(fragmentShader);
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        console.error('ERROR compiling fragment Shader', gl.getShaderInfoLog(fragmentShader));
+        return;
+    }
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+        return;
+    }
+
+    gl.validateProgram(program);
+    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+        console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+        return;
+    }
+
+    var triangleVertices =
+        [ //X, Y
+
+        ];
+
+    var triangleVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+
+    var positionAttributeLocation = gl.getAttribLocation(program, 'vertPosition');
+    gl.vertexAttribPointer(
+        positionAttributeLocation,
+        2,
+        gl.FLOAT,
+        gl.FALSE,
+        2 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    );
+
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    gl.useProgram(program);
+    gl.drawArrays(); // set the type of the poly and the number of vertices
 }
